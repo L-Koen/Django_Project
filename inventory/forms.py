8dec9d9d2133369c918af31b5f4349547a2bf31a
L_Koen <lauw.koen@gmail.com>
2023-04-26 18:05:34 +0200

    Started creating views and some html to check the whole chain.

--- inventory/forms.py
+++ inventory/forms.py
@@ -5,4 +5,70 @@ from .models import Ingredient, MenuItem, RecepyRequirement, Purchase
 class IngredientCreateForm(forms.Form):
     class meta:
         model = Ingredient
-        fields = ["name", "quantity", "unit", "unit_price"]
+        fields = "__all__"
+
+
+class IngredientUpdateForm(forms.form):
+    class meta:
+        model = Ingredient
+        fields = ["quantity", "unit_price"]
+
+
+class IngredientForm(forms.Form):
+    class meta:
+        model = Ingredient
+        fields = "__all__"
+
+
+class MenuItemCreateForm(forms.Form):
+    class meta:
+        model = MenuItem
+        fields = "__all__"
+
+
+class MenuItemUpdateForm(forms.Form):
+    class meta:
+        model = MenuItem
+        fields = "__all__"
+
+
+class MenuItemForm(forms.Form):
+    class meta:
+        model = MenuItem
+        fields = "__all__"
+
+
+class RecepyRequirementCreateForm(forms.Form):
+    class meta:
+        model = RecepyRequirement
+        fields = "__all__"
+
+
+class RecepyRequirementUpdateForm(forms.Form):
+    class meta:
+        model = RecepyRequirement
+        fields = "__all__"
+
+
+class RecepyRequirementForm(forms.Form):
+    class meta:
+        model = RecepyRequirement
+        fields = "__all__"
+
+
+class PurchaseCreateForm(forms.Form):
+    class meta:
+        model = Purchase
+        fields = "__all__"
+
+
+class PurchaseUpdateForm(forms.Form):
+    class meta:
+        model = Purchase
+        fields = "__all__"
+
+
+class PurchaseForm(forms.Form):
+    class meta:
+        model = Purchase
+        fields = "__all__"
--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.urls import reverse
 from .models import Ingredient, MenuItem, RecepyRequirement, Purchase
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import DeleteView, CreateView, UpdateView
 
 
 # Start to create views here
+class HomeView(TemplateView):
+    template_name = "inventory/home.html"
+
+
+# Create your views here.
+class MenuItemView(ListView):
+    model = MenuItem
+    template_name = "inventory/menuitems.html"
